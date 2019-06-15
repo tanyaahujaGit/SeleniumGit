a505447bff3cad6d97e1e24930224f9dd748b67f
tanyaahujaGit <tanyaahuja_112@outlook.com>
2019-06-15 15:46:15 +0530

    First commit

---
+++ src/test/java/UdemyMaven/AppiumTest.java
@@ -0,0 +1,19 @@
+package UdemyMaven;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void AndroidApp()
+	{
+		System.out.println("AndroidApp");
+	}
+	
+	@Test
+	public void IosApp()
+	{
+		System.out.println("IosApp");
+	}
+
+}
---
+++ src/test/java/UdemyMaven/RESTAPITest.java
@@ -0,0 +1,19 @@
+package UdemyMaven;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	
+	@Test
+	public void PostJira()
+	{
+		System.out.println("PostJira");
+	}
+	
+	@Test
+	public void DeleteTwitter()
+	{
+		System.out.println("DeleteTwitter");
+	}
+
+}
